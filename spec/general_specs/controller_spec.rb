a8716698c49b9900ef5b370f26e9c851506bc381
vitorascorrea <vitorascorrea@gmail.com>
2016-10-21 01:20:00 +0000

    installed capybara

---
+++ spec/general_specs/controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe IngredientsController, type: :controller do
+  describe 'should show only index (since its a SPA)' do
+    before do
+      get :index, format: :json
+    end
+    it { expect(response).to be_success}
+  end
+end
